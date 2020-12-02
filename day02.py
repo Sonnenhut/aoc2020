f4df6580b4ff3c30a757e51798ea2ef8aa304d5b
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-02 08:29:11 +0100

    Day 2

---
+++ day02.py
@@ -0,0 +1,32 @@
+input = open("inputs/day02.txt", "r").read().splitlines()
+
+
+def part1():
+    cnt = 0
+    for line in input:
+        line = line.replace("\n", "")
+        [desc, pw] = line.split(": ")
+        [amount, char] = desc.split(" ")
+        [minv, maxv] = amount.split("-")
+
+        if int(maxv) >= pw.count(char) >= int(minv):
+            cnt += 1
+    return cnt
+
+
+def part2():
+    cnt = 0
+    for line in input:
+        line = line.replace("\n", "")
+        [desc, pw] = line.split(": ")
+        [amount, char] = desc.split(" ")
+        [pos1, pos2] = amount.split("-")
+        if (pw[int(pos1) - 1] == char) != (pw[int(pos2) - 1] == char):
+            cnt += 1
+    return cnt
+
+
+#582
+print(part1())
+#729
+print(part2())
