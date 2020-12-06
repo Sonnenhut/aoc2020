45cfd77f23957ff4c646db9b6b89a1812f7d3925
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-06 09:09:01 +0100

    Day 5

---
+++ day05.py
@@ -0,0 +1,44 @@
+inp = open("inputs/day05.txt", "r").read().splitlines()
+
+def binary_space_num(desc, base):
+    minv = 1
+    maxv = base
+    for char in desc:
+        space = maxv - minv + 1
+        if char == 'F' or char == 'L':
+            maxv = maxv - (space / 2)
+        elif char == 'B' or char == 'R':
+            minv = minv + (space / 2)
+
+        if minv == maxv:
+            return int(minv) - 1
+
+    raise ValueError
+
+
+def seat_id(desc):
+    return binary_space_num(desc[:7], 128) * 8 + binary_space_num(desc[7:10], 8)
+
+
+def part1():
+    return max(map(seat_id, inp))
+
+
+def part2():
+    seats = sorted(list(map(seat_id, inp)))
+    res = -1
+    for idx in range(min(seats), max(seats) + 1):
+        if idx not in seats:
+            res = idx
+    return res
+
+# 913
+print(part1())
+# not 4, not 8
+print(part2())
+
+# 1 - 128
+# 1-64, 65-128
+# 1-32,33-64,  65-97,98-128
+
+# test if code works for row 0 FFFFFFFF
