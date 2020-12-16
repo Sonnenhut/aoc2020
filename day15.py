737cd6b5167e4be62ca373ae815c70203ce149ea
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-16 21:54:57 +0100

    Day 15 Some small improvements on runtime

--- day15.py
+++ day15.py
@@ -1,22 +1,15 @@
 import sys
+import itertools
 
 inp = list(map(int, open("inputs/day15.txt").read().replace("\n", "").split(",")))
 
 
 def spoken_at_turn(wanted_turn):
-    return next(x for i, x in enumerate(gen(inp), 1) if i == wanted_turn)
-
-
-def part1():
-    return spoken_at_turn(2020)
-
-
-def part2():
-    return spoken_at_turn(30000000)
+    return next(itertools.islice(gen(inp), wanted_turn - 1, None))
 
 
 def gen(initial):
-    memory = {}
+    memory = [-1] * 30000000
     for iturn, speak in enumerate(initial, 1):
         memory[speak] = iturn
         yield speak
@@ -25,8 +18,8 @@ def gen(initial):
     spoken_last = initial[-1]
     for turn in range(start_turn, sys.maxsize):
         last_turn = turn - 1
-        spoken_memory = memory.get(spoken_last, None)
-        if spoken_memory is None or turn == start_turn:
+        spoken_memory = memory[spoken_last]
+        if spoken_memory == -1:
             speak = 0
         else:
             speak = last_turn - spoken_memory
@@ -37,7 +30,7 @@ def gen(initial):
         spoken_last = speak
 
 
-# 610
-print(part1())
-# 1407
-print(part2())
+# part1_ 610
+spoken_at_turn(2020)
+# part2: 1407
+spoken_at_turn(30000000)
