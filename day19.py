4f729ab4d569b4740b3f3c94534615a8eb6f5584
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-19 19:17:04 +0100

    Day 19

--- day19.py
+++ day19.py
@@ -2,48 +2,80 @@ import re
 
 
 def part1():
-    blocks = open("inputs/day19.txt").read().split("\n\n")
+    blocks = open("inputs/day19t.txt").read().split("\n\n")
     rules = parse_rules(blocks[0])
     tests = blocks[1].splitlines()
     res = 0
     for test in tests:
-        matches, remainder = is_valid(test, rules, 0)
-        res += matches and len(remainder) == 0
+        res += is_valid(test, rules, 0)
     return res
-    #return sum([is_valid(test, rules, 0) for test in tests])0
 
 
-def is_valid(test, rules, idx):
-    possibles = [rules[idx]]
-    if "|" in rules[idx]:
-        possibles = rules[idx].split("|")
+def part2():
+    blocks = open("inputs/day19t.txt").read().split("\n\n")
+    rules = parse_rules(blocks[0])
+    tests = blocks[1].splitlines()
+    rules[8] = "42 | 42 8"
+    rules[11] = "42 31 | 42 11 31"
+    res = 0
+    for test in tests:
+        res += is_valid(test, rules, 0)
+    return res
 
-    one_side_matches = False
-    test_remain = test
-    while one_side_matches is False and len(possibles):
-        test_remain = test
-        possible_matches = True
-        for c in possibles.pop():
+
+def is_valid(test, rules, rule_idx):
+    rule = rules[rule_idx]
+
+    remainder = test
+    for delegate in rule.strip().split(" "):
+        found = check_recursive(remainder, rules, int(delegate))
+        #print("rule {} found {}".format(delegate, found))
+        if len(found):
+            remainder = remainder[len(found):]
+        else:
+            break
+    return len(remainder) == 0
+
+
+def check_recursive(test, rules, rule_idx):
+    rule = rules[rule_idx]
+    match = []
+    subrules = rule.split("|")
+    while subrules:
+        check = subrules.pop()
+        match = []
+        for c in check.strip().split(" "):
             if c.isdigit():
-                valid, test_remain = is_valid(test_remain, rules, int(c))
-                possible_matches &= valid
-                if not possible_matches:
-                    break
-            elif c == " ":
-                continue
-            elif c.isalpha():
-                if len(test) == 0:
-                    return False, test
+                match_part = check_recursive(test[len(match):], rules, int(c))
+                if len(match_part):
+                    match += match_part
                 else:
-                    return test[0] == c, test[min(len(test), 1):]
-        one_side_matches |= possible_matches
-    return one_side_matches, test_remain
+                    match = []
+                    break
+            elif c.isalpha():
+                if test[0] == c:
+                    return test[0]
+                else:
+                    return ""
+        if match:
+            # when one subrule matches, don't even try looking at the other one...
+            subrules = []
+    #print("return match for rule", rule, match)
+    return match
 
 
 def parse_rules(text):
     lines = sorted(text.splitlines(), key=lambda line: int(line.split(":")[0]))
-    rules = [line.split(":")[1].replace(" ", "") for line in lines]
-    return rules
+    res = {int(line.split(":")[0]): line.split(":")[1].replace("\"", "") for line in lines}
+    return res
 
 
-print(part1())
+# 171
+#print(part1())
+
+
+print("part2",part2())
+
+
+exec('def foo(): print("test")')
+exec("foo()")
