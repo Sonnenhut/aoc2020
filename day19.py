9fc176430717a715b660fd02000ce2211cffc0e2
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-20 01:53:01 +0100

    Day 19 implemented by expanding rules and turning it into a regex

--- day19.py
+++ day19.py
@@ -2,80 +2,58 @@ import re
 
 
 def part1():
-    blocks = open("inputs/day19t.txt").read().split("\n\n")
-    rules = parse_rules(blocks[0])
-    tests = blocks[1].splitlines()
-    res = 0
-    for test in tests:
-        res += is_valid(test, rules, 0)
-    return res
+    blocks = open("inputs/day19.txt").read().split("\n\n")
+    rgx = expand(parse_rules(blocks[0]))
+    return sum([re.match(rgx, t) is not None for t in blocks[1].splitlines()])
 
 
 def part2():
-    blocks = open("inputs/day19t.txt").read().split("\n\n")
+    blocks = open("inputs/day19.txt").read().split("\n\n")
     rules = parse_rules(blocks[0])
-    tests = blocks[1].splitlines()
-    rules[8] = "42 | 42 8"
-    rules[11] = "42 31 | 42 11 31"
-    res = 0
-    for test in tests:
-        res += is_valid(test, rules, 0)
-    return res
 
+    # 8: 42 | 42 8
+    # loops 42 at least one or more times
+    rules[8] = "( 42 ){1,}"
 
-def is_valid(test, rules, rule_idx):
-    rule = rules[rule_idx]
+    # 11: 42 31 | 42 11 31
+    # basically 42 31 loops where it can occur again: "42 (42 (..) 31) 31", but has to occur at least like "42 31"
+    # not really "infinite" as per description, but matches all examples just as fine
+    rules[11] = "( 42 ( 42 ( 42 ( 42 ( 42 ( 42 31 ){0,1} 31 ){0,1} 31 ){0,1} 31 ){0,1} 31 ){0,1} 31 ){1}"
 
-    remainder = test
-    for delegate in rule.strip().split(" "):
-        found = check_recursive(remainder, rules, int(delegate))
-        #print("rule {} found {}".format(delegate, found))
-        if len(found):
-            remainder = remainder[len(found):]
-        else:
-            break
-    return len(remainder) == 0
-
-
-def check_recursive(test, rules, rule_idx):
-    rule = rules[rule_idx]
-    match = []
-    subrules = rule.split("|")
-    while subrules:
-        check = subrules.pop()
-        match = []
-        for c in check.strip().split(" "):
-            if c.isdigit():
-                match_part = check_recursive(test[len(match):], rules, int(c))
-                if len(match_part):
-                    match += match_part
-                else:
-                    match = []
-                    break
-            elif c.isalpha():
-                if test[0] == c:
-                    return test[0]
-                else:
-                    return ""
-        if match:
-            # when one subrule matches, don't even try looking at the other one...
-            subrules = []
-    #print("return match for rule", rule, match)
-    return match
+    rgx = expand(rules)
+    return sum([re.match(rgx, test) is not None for test in blocks[1].splitlines()])
 
 
 def parse_rules(text):
     lines = sorted(text.splitlines(), key=lambda line: int(line.split(":")[0]))
-    res = {int(line.split(":")[0]): line.split(":")[1].replace("\"", "") for line in lines}
-    return res
+    size = int(lines[-1].split(":")[0]) + 1
+
+    rules = [""] * size
+    rules[0] = lines[0].split(": ")[1]
+    for line in lines[1:]:
+        idx, line = line.split(": ")
+        idx = int(idx)
+        line = line.replace("\"", "")
+        if "|" in line:
+            rules[idx] = "( " + line + " )"
+        else:
+            rules[idx] = line
+    return rules
+
+
+def expand(rules):
+    while any(c.isdigit() for c in rules[0].split(" ")):
+        expanded = []
+        for c in rules[0].split(" "):
+            newc = c
+            if c.isdigit():
+                newc = rules[int(c)]
+            expanded.append(newc)
+        rules[0] = " ".join(expanded)
+    return "^" + rules[0].replace(" ", "") + "$"
 
 
 # 171
-#print(part1())
-
-
-print("part2",part2())
-
-
-exec('def foo(): print("test")')
-exec("foo()")
+print(part1())
+# 369
+print(part2())
