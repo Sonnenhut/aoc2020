81dfefc4d8fa5394de57cea648619637e72b7321
Sonnenhut <991312+Sonnenhut@users.noreply.github.com>
2020-12-23 15:21:31 +0100

    Day 23 with array implementation

--- day23.py
+++ day23.py
@@ -1,12 +1,12 @@
 from functools import reduce
 
 inp = [int(c) for c in open("inputs/day23.txt").read().splitlines()[0]]
-#inp = [int(c) for c in "389125467"]
+# inp = [int(c) for c in "389125467"]
 
 
 class Game:
     def __init__(self, arr):
-        self.world = {}
+        self.world = [0] * (len(arr)+1)
         for key, value in zip(arr, arr[1:] + arr[0:1]):
             self.world[key] = value
         self.csr = arr[0]
@@ -14,9 +14,6 @@ class Game:
     def tick(self):
         picked = self.take()
         dest = self.destination(picked)
-
-        #print("taken", picked)
-        #print("destination", dest)
 
         # slice out the taken bits
         self.world[self.csr] = self.world[picked[-1]]
@@ -29,7 +26,7 @@ class Game:
     def destination(self, ignore):
         i = self.csr - 1
         if i <= 0:
-            i = max(self.world.keys())#self.end
+            i = len(self.world) - 1
 
         while True:
             if i not in ignore:
@@ -37,14 +34,14 @@ class Game:
 
             i -= 1
             if i <= 0:
-                i = max(self.world.keys())
+                i = len(self.world) - 1
         return i
 
     def take(self):
         n1 = self.world[self.csr]
         n2 = self.world[n1]
         n3 = self.world[n2]
-        return [n1, n2, n3]
+        return n1, n2, n3
 
     def labels_after_1(self, amt):
         res = []
@@ -54,18 +51,10 @@ class Game:
             csr = self.world[csr]
         return res
 
-    def __str__(self):
-        res = ["({})".format(self.csr)]
-        csr = self.csr
-        while len(res) != len(self.world.keys()):
-            res += [str(self.world[csr])]
-            csr = self.world[csr]
-        return " ".join(res)
-
 
 def part1():
     game = Game(inp)
-    for i in range(100):
+    for _ in range(100):
         game.tick()
     return "".join(str(i) for i in game.labels_after_1(len(inp) - 1))
 
@@ -73,10 +62,8 @@ def part1():
 def part2():
     inp_patched = inp + list(range(max(inp) + 1, 1_000_001))
     game = Game(inp_patched)
-    for i in range(10_000_000):
+    for _ in range(10_000_000):
         game.tick()
-        if i % 1_000_000 == 0:
-            print(i)
 
     return reduce(lambda acc, v: acc * v, game.labels_after_1(2))
 
